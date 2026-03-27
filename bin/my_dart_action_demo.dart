2eff430fc787f361005d448842f954eea643f2e3
Deepak <deepak.guptha.s@outlook.com>
2026-03-27 08:55:30 -0400

    initial commit

---
+++ bin/my_dart_action_demo.dart
@@ -0,0 +1,5 @@
+import 'package:my_dart_action_demo/my_dart_action_demo.dart' as my_dart_action_demo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_dart_action_demo.calculate()}!');
+}
---
+++ test/my_dart_action_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_dart_action_demo/my_dart_action_demo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
